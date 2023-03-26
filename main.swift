65fbc4a8488f877e088f1b4006579a53e861a3f8
Pedro Henrique <83087869+PeedroHenriquee@users.noreply.github.com>
2023-03-26 13:43:57 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,16 @@
+//Faça um programa que calcula e mostra o salário de um funcionário que teve um aumento de 25%
+
+import Foundation
+
+//variavél que recebe o valor do salario
+
+let Salario: Float = 1500
+
+//logica para calcular o aumento
+let Aumento: Float = ((25 / 100) * Salario) + Salario
+
+//imprimi na tela o aumento de salario
+print(Aumento)
+
+
+
